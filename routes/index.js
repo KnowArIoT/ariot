7f92baba97ae4dde87e9db74f825467d4e8a4ff2
Thuc Hoang <thuc.hoang@knowit.no>
2017-03-11 00:53:20 +0100

    Fixed retrieval

--- routes/index.js
+++ routes/index.js
@@ -72,7 +72,7 @@ router.get('/data/:sensor', (req, res, next) => {
 
 router.get('/image', (req, res, next) => {
   database.getImageData().then((result) => {
-    res.status(200).json(results);
+    res.status(200).json(result);
   })
   .catch((err) => {
     res.status(500).send(err);
